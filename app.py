a8e1dd7326f6e28b131e947a44c61e953acefead
Ivan Loh <ivan.loh@fwd.com>
2022-03-30 06:24:27 +0000

    added pymssql example

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print('hello mssql')
+import os
+import pymssql
+
+server = 'localhost'
+user = 'sa'
+password = os.environ.get('SA_PASSWORD')
+
+conn = pymssql.connect(server, user, password, 'tempdb')
+cursor = conn.cursor()
+cursor.execute('SELECT 1 AS count')
+for row in cursor:
+    print(row)
+
+conn.close()
