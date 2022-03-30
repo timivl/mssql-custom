9fbf1b0b1088de14ceb0cdb40d9b1458e8bcc6cd
Ivan Loh <ivan.loh@fwd.com>
2022-03-30 06:35:23 +0000

    Added lazy retry

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 import os
+import time
 import pymssql
 
-server = 'localhost'
-user = 'sa'
-password = os.environ.get('SA_PASSWORD')
 
-conn = pymssql.connect(server, user, password, 'tempdb')
+
+def connect():
+
+    server   = os.environ.get('DB_HOST', 'localhost')
+    user     = os.environ.get('DB_USER', 'sa')
+    password = os.environ.get('SA_PASSWORD')
+
+    try:
+        conn = pymssql.connect(server, user, password, 'tempdb')
+        return conn
+    except:
+        time.sleep(3)
+        print('retrying connection to database')
+        return connect()
+
+
+conn   = connect()
 cursor = conn.cursor()
 cursor.execute('SELECT 1 AS count')
 for row in cursor:
